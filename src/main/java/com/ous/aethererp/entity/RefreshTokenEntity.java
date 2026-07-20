a6a158c1a2ba54d31aebed83092a1fe25b1664e5
Ousmane Sangary <sangaryousmane@gmail.com>
2026-07-20 21:43:49 +0000

    Add the refresh tokens entity

---
+++ src/main/java/com/ous/aethererp/entity/RefreshTokenEntity.java
@@ -0,0 +1,42 @@
+package com.ous.aethererp.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tbl_refresh_tokens")
+@Setter @Getter @NoArgsConstructor @AllArgsConstructor @Builder
+public class RefreshTokenEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     *  Random refresh token
+     */
+    @Column(nullable = false, unique = true, length = 500)
+    private String token;
+
+    /**
+     *  Token expiration time
+     */
+    @Column(nullable = false)
+    private Long expiryDate;
+
+    /**
+     *  Whether this token has been revoked
+     */
+    @Column(nullable = false)
+    private Boolean isTokenRevoked;
+
+
+    /**
+     *  Many refresh tokens can belong to one user.
+     *  E.g. One user login through multiple devices.
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+}
