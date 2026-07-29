6b7c79756c186e7dcc635e313fd423cd349d31df
Ousmane Sangary <sangaryousmane@gmail.com>
2026-07-29 17:33:37 +0000

    Configure spring mvc to serve the images

--- src/main/java/com/ous/aethererp/config/WebConfig.java
+++ src/main/java/com/ous/aethererp/config/WebConfig.java
@@ -1,5 +1,16 @@
 package com.ous.aethererp.config;
 
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/");
+    }
 }
